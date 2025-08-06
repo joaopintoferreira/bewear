f8aac5a6ea8eaab42516cb04065c591173b244c3
João Pinto Ferreira <joaopintoferreira6@gmail.com>
2025-08-06 01:21:32 -0300

    autenticaçao

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import { Header } from "@/components/common/header";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import SignInForm from "./components/sign-in-form";
 import SignUpForm from "./components/sign-up-form";
 
-const Authentication = async () => {
+const Authentication = () => {
   return (
     <>
       <Header />
